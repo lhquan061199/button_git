dcd1dbd2792e5d3a0a6e36f2fd043234ea5f8d75
lequan <lequan0611@gmail.com>
2023-07-24 16:27:33 +0700

    clean code

--- index.js
+++ index.js
@@ -1,21 +1,7 @@
 const buttons = document.querySelectorAll(".btn");
-const button1 = document.querySelector(".btn-primary");
-console.log("🚀 ~ file: index.js:3 ~ button1:", button1);
 
-console.log(button1);
-
-// const handleClick = () => {
-//   console.log("aaaaaaaaaaaaaaaa");
-// };
-
-// button1.onclick = () => {
-//   console.log(1);
-// };
-
-let myArray = Array.from(buttons);
-
-myArray.map((item) => {
-  item.onclick = (e) => {
+buttons.forEach((item) => {
+  item.onclick = () => {
     alert(item.innerText);
   };
 });
